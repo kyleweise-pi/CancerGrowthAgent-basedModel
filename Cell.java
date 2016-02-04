6a1fa85855e00356cec381ff97aaa41f977bcf62
kyleweise <weisek@my.easternct.edu>
2016-02-03 23:10:50 -0500

    Create Cell.java

---
+++ Cell.java
@@ -0,0 +1,16 @@
+package cancerModel;
+
+public class Cell {
+	
+	public int x; //x position of cell
+	public int y; //y position of cell
+	public char type; //type of cell
+
+	
+	public Cell(int x, int y, char type)
+	{
+		this.x = x;
+		this.y = y;
+		this.type = type;
+	}
+}
